cd3a0391d963e90c9e0174e4876fa392a15c1299
Aerendir <hello@aerendir.me>
2015-07-17 12:55:06 +0200

    #4. Add a Phone Number Value Object.
    Update composer.json adding giggsey/libphonenumber-for-php.
    Add files for Value Object.

---
+++ src/Phone/Phone.php
@@ -0,0 +1,43 @@
+<?PHP
+
+/**
+ *  A Phone value object.
+ *
+ * This is a simple wrapper for giggsey/libphonenumber-for-php
+ * (https://github.com/giggsey/libphonenumber-for-php)
+ *
+ * @package  Serendipity\Framework
+ * @subpackage ValueObjects
+ *
+ *  @author      Adamo Crespi <hello@aerendir.me>
+ *  @copyright   Copyright (c) 2015, Adamo Crespi
+ *  @license     MIT License
+ */
+
+namespace SerendipityHQ\Framework\ValueObjects\Phone;
+
+use SerendipityHQ\Framework\ValueObjects\Phone\PhoneInterface;
+
+use libphonenumber\PhoneNumber;
+use libphonenumber\PhoneNumberUtil;
+
+class Phone extends PhoneNumber implements PhoneInterface
+{
+    protected $phone;
+
+    public function __construct($phone = null, $region = 'EN')
+    {
+        if (is_string($phone))
+        {
+            $phoneUtil = PhoneNumberUtil::getInstance();
+            $phone = $phoneUtil->parse($phone, $region);
+        }
+
+        if ($phone instanceof PhoneNumber)
+        {
+            $this->mergeFrom($phone);
+        }
+    }
+
+    public function __set($field, $value){}
+}
---
+++ src/Phone/PhoneInterface.php
@@ -0,0 +1,19 @@
+<?PHP
+
+/**
+ *  A Phone value object
+ *
+ * @package  Serendipity\Framework
+ * @subpackage ValueObjects
+ *
+ *  @author      Adamo Crespi <hello@aerendir.me>
+ *  @copyright   Copyright (c) 2015, Adamo Crespi
+ *  @license     MIT License
+ */
+
+namespace SerendipityHQ\Framework\ValueObjects\Phone;
+
+use SerendipityHQ\Framework\ValueObjects\Common\ValueObjectInterface;
+
+interface PhoneInterface extends ValueObjectInterface
+{}
