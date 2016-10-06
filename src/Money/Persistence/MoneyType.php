86831c21ff1af77510e1e0743e4d267b0a9c79ce
Aerendir <hello@aerendir.me>
2016-10-06 17:34:50 +0200

    #19. Create a Doctrine custom type for `Money`.

---
+++ src/Money/Persistence/MoneyType.php
@@ -0,0 +1,63 @@
+<?php
+namespace SerendipityHQ\Component\ValueObjects\Money\Persistence;
+
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use SerendipityHQ\Component\ValueObjects\Currency\Currency;
+use SerendipityHQ\Component\ValueObjects\Money\Money;
+
+/**
+ * A custom datatype to persist a Money Value Object with Doctrine.
+ *
+ * @author Adamo Crespi <hello@aerendir.me>
+ */
+class MoneyType extends Type
+{
+    const MONEY = 'money';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return $platform->getVarcharTypeDeclarationSQL($fieldDeclaration);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefaultLength(AbstractPlatform $platform)
+    {
+        return $platform->getVarcharDefaultLength();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        $objects = explode('-', $value);
+
+        $currency = new Currency($objects[1]);
+
+        return new Money(['amount' => $objects[0], 'currency' => $currency]);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param Money $value
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        return $value->getAmount() . '-' . $value->getCurrency()->getCurrencyCode();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return self::MONEY;
+    }
+}
---
+++ tests/Money/Persistence/MoneyTypeTest.php
@@ -0,0 +1,31 @@
+<?PHP
+
+/**
+ *  A Money value object.
+ *
+ * This is a simple wrapper for giggsey/libphonenumber-for-php
+ * (https://github.com/giggsey/libphonenumber-for-php)
+ *
+ *
+ *  @author      Adamo Crespi <hello@aerendir.me>
+ *  @copyright   Copyright (c) 2015, Adamo Crespi
+ *  @license     MIT License
+ */
+namespace SerendipityHQ\Component\ValueObjects\tests\Money\Persistence;
+
+use SerendipityHQ\Component\ValueObjects\Common\ComplexValueObjectInterface;
+use SerendipityHQ\Component\ValueObjects\Currency\CurrencyInterface;
+use SerendipityHQ\Component\ValueObjects\Money\Money;
+use SerendipityHQ\Component\ValueObjects\Money\MoneyInterface;
+use SerendipityHQ\Component\ValueObjects\Money\Persistence\MoneyType;
+
+/**
+ * @author Adamo Crespi <hello@aerendir.me>
+ */
+class MoneyTypeTest extends \PHPUnit_Framework_TestCase
+{
+    public function testMoneyType()
+    {
+        $this->markTestSkipped('See http://stackoverflow.com/questions/39900136/how-to-test-doctrine-custom-types');
+    }
+}
