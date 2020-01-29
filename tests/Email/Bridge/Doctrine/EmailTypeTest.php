f48d58e158088d2d3452eece35480f33ef2f6240
Aerendir <hello@aerendir.me>
2020-01-29 12:16:11 +0100

    Complete unit tests for `Email`.

--- tests/Email/Bridge/Doctrine/EmailTypeTest.php
+++ tests/Email/Bridge/Doctrine/EmailTypeTest.php
@@ -12,18 +12,85 @@
 
 namespace SerendipityHQ\Component\ValueObjects\Tests\Email\Bridge\Doctrine;
 
+use Doctrine\DBAL\Platforms\AbstractPlatform;
 use PHPUnit\Framework\TestCase;
+use SerendipityHQ\Component\ValueObjects\Email\Bridge\Doctrine\EmailType;
+use SerendipityHQ\Component\ValueObjects\Email\Email;
 
 /**
  * @author Adamo Crespi <hello@aerendir.me>
  */
 class EmailTypeTest extends TestCase
 {
-    /**
-     * @doesNotPerformAssertion
-     */
-    public function testEmailType()
+    /** @var EmailType $type */
+    private $type;
+
+    /** @var AbstractPlatform $platform */
+    private $platform;
+
+    protected function setUp(): void
     {
-        $this::markTestSkipped('See http://stackoverflow.com/questions/39900136/how-to-test-doctrine-custom-types');
+        $typeBuilder    = $this->getMockBuilder(EmailType::class)->disableOriginalConstructor()->setMethods(null);
+        $this->type     = $typeBuilder->getMock();
+        $this->platform = $this->getMockForAbstractClass(AbstractPlatform::class);
+    }
+
+    public function testGetName()
+    {
+        $name = $this->type->getName();
+        $this->assertSame('email', $name);
+    }
+
+    public function testConvertToPHPValue()
+    {
+        $result = $this->type->convertToPHPValue('hello@serendipityhq.com', $this->platform);
+        $this->assertInstanceOf(Email::class, $result);
+    }
+
+    public function testConvertToPHPValueHandlesNull()
+    {
+        $result = $this->type->convertToPHPValue(null, $this->platform);
+        $this->assertNull($result);
+    }
+
+    public function testConvertToPHPValueHandlesEmptyString()
+    {
+        $result = $this->type->convertToPHPValue('', $this->platform);
+        $this->assertNull($result);
+    }
+
+    public function testConvertToDatabaseValue()
+    {
+        $emailValue = 'hello@aexample.com';
+        $email      = new Email($emailValue);
+        $result     = $this->type->convertToDatabaseValue($email, $this->platform);
+        $this->assertSame($emailValue, $result);
+    }
+
+    public function testConvertToDatabaseValueHandlesNull()
+    {
+        $result = $this->type->convertToDatabaseValue(null, $this->platform);
+        $this->assertNull($result);
+    }
+
+    public function testConvertToDatabaseValueHandlesEmptyString()
+    {
+        $result = $this->type->convertToDatabaseValue('', $this->platform);
+        $this->assertNull($result);
+    }
+
+    public function testConvertToDatabaseValueAcceptsOnlyEmailObjects()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->type->convertToDatabaseValue('hello@serendipityhq.com', $this->platform);
+    }
+
+    public function testConvertToDatabaseValueValidatesEmail()
+    {
+        $mockEmail = $this->createMock(Email::class);
+        $mockEmail->method('__toString')->willReturn('hello-serendipityhq.com');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->type->convertToDatabaseValue($mockEmail, $this->platform);
     }
 }
